16abd01a7dfa320a4410e36e765fd4d98c400909
Ukpe Sophia Abel <54262136+SophiannaRoxanna@users.noreply.github.com>
2021-10-18 19:34:59 +0100

    worked on Orders, added the search and filter

--- resources/js/Pages/Orders/Index.vue
+++ resources/js/Pages/Orders/Index.vue
@@ -9,7 +9,7 @@
             <div class="flex items-center">
               <div>
                 <div class="flex items-center">
-                  <h1 class=" ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate" > Orders </h1>
+                  <h1 class="text-2xl font-semibold leading-7 text-blue-gray-900 sm:leading-9 sm:truncate" >Orders</h1>
                 </div>
               </div>
             </div>
@@ -18,66 +18,45 @@
             <inertia-link
               href="orders/create"
               type="button"
-              class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
-              Create New Order
+              class="flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+              <span class="pr-2 mb-0.5"><PlusIcon class="w-5 h-5" /></span>
+              Create Order
             </inertia-link>
           </div>
         </div>
-          <div class="pb-5 border-b border-gray-200 sm:pb-0">
-          <div class="mt-3 sm:mt-4">
-            <div class="sm:hidden">
-              <label for="current-tab" class="sr-only">Select a tab</label>
-              <select id="current-tab" name="current-tab" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
-              </select>
-            </div>
-            <div class="hidden sm:block">
-              <nav class="-mb-px flex space-x-8">
-                <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'whitespace-nowrap pb-1.5 px-1 border-b-2 font-medium text-sm']" :aria-current="tab.current ? 'page' : undefined">
-                 <div v-if="tab.name=='All'">
-                  <span class="flex"> {{ tab.name }}</span>
-                 </div>
-                 <div v-else>
-                  <span class="flex"> {{ tab.name }}  </span>
-                 </div>
-                </a>
-              </nav>
-            </div>
+        <div class="flex items-center justify-between">
+          <div>
+            <h2 class="text-gray-900 text-xl leading-6 font-medium mr-2">All Orders()</h2>
           </div>
-          </div>  
-          <div class="flex justify-between">
-            <div class="relative mt-5">
-              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
-                  <svg fill="none" stroke="#666666" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-5 h-5"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                </button>
-              </span>
-              <div class="min-w-0 flex-1">
-                <label for="search" class="sr-only">Search</label>
-                <input id="text" type="text" placeholder="Search for products..." class="block pl-10 py-2.5 rounded-md border-1 border-gray-300 text-xs text-gray-900 placeholder-gray-500 focus:outline-none bg-transparent"/>
-              </div>
-            </div>
-            <div class="mt-6 flex">
+          <div class="flex items-center justify-end">
+            <search 
+              :suggestions="suggestions"
+              @autocomplete="getAutoCompleteData"
+              @update-current-list="updateCurrentList"
+              :selection="selection"
+            ></search>
+          </div>
+        </div>
+            <!-- <div class="mt-6 flex"> -->
              <!--  <div class="">
                 <button class="">
                   <input type="date" name="" id="theDate">
                 </button>
               </div> -->
-              <datepicker
+              <!-- <datepicker
                 v-model="form.from"
                 class="date-filter text-black focus:outline-none w-36 rounded-md border-1 border-gray-300"
                 date-format="Y-m-d"
                 user-format="Y-m-d"
-              />
-              <arrow-right class="my-auto mx-3"/>
+              /> -->
               <!-- <datepicker
                 v-model="form.to"
                 class="date-filter text-black focus:outline-none w-36 rounded-md border-1 border-gray-300 to"
                 date-format="Y-m-d"
                 user-format="Y-m-d"
               /> -->
-            </div>
-          </div>
+            <!-- </div> -->
+          <!-- </div> -->
       </div>
     </div>
 
@@ -85,7 +64,7 @@
       <!-- Activity table (small breakpoint and up) -->
       <div class="hidden sm:block">
         <div class="mx-auto px-4 sm:px-6 lg:px-">
-          <Search v-bind:suggestions="suggestions"></Search>
+          <!-- <Search v-bind:suggestions="suggestions"></Search> -->
           <div class="flex flex-col mt-2">
             <div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200 table-fixed">
@@ -95,13 +74,13 @@
                         <input id="comments" aria-describedby="comments-description" name="comments" type="checkbox" v-model="selectAll" class="h-4 w-4 text-indigo-600 border-gray-300 rounded" @click="checkAll" :disabled="orders.data.length === 0" />
                     </th> 
                     <th scope="col" class="w-1/12 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ">
-                      OrderID
+                      Order No.
                     </th>
                     <th scope="col" class="w-1/6 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Customer
+                      Date
                     </th>
                     <th scope="col" class="w-1/12 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      To
+                      Customer
                     </th>
                     <th scope="col" class="w-1/6 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Items
@@ -131,11 +110,11 @@
                     <td class="w-1/12 pr-7 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider " >
                       {{ order.id }}
                     </td>
-                    <td class="w-1/6 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ">
-                      {{ order.user.first_name }} {{ order.user.last_name }}
-                    </td>
                     <td class="w-1/12 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       {{ order.country }}
+                    </td>
+                    <td class="w-1/6 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ">
+                      {{ order.user.first_name }} {{ order.user.last_name }}
                     </td>
                     <td class="w-1/6 px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" >
                       {{order.items.length == 1 ? "1 item": order.items.length + "Items"}}
@@ -259,12 +238,14 @@
 <script>
 import { ref } from 'vue'
 import AppLayout from "../../Layouts/AppLayout.vue";
-import { ChevronDownIcon,ChevronUpIcon } from "@heroicons/vue/solid"
+import { ChevronDownIcon,ChevronUpIcon, PlusIcon } from "@heroicons/vue/solid"
+import { FilterIcon } from "@heroicons/vue/outline"
 import Input from '../../Jetstream/Input.vue';
 import ArrowRight from '../../../assets/ArrowRight.vue';
 // import Datepicker from 'vue3-datepicker'
-// import Search from '../Search.vue'
+import Search from "../Search.vue"
 // import axios from "axios"
+
 const tabs = [
   { name: 'All', href: '#', current: true },
   { name: 'Fulfilment', href: '#', current: false },
@@ -423,7 +404,11 @@ export default {
     ChevronDownIcon,
     Input,
     ArrowRight,
-    Datepicker
+    PlusIcon,
+    FilterIcon,
+    Search,
+    
+    // Datepicker,
     // SideNav,
     // ExportIcon,
     // ImportIcon,
@@ -443,9 +428,32 @@ export default {
   
   setup() {
     const open = ref(false);
-    console.log(tabs.selected)
+    const suggestions = ref([]);
+    const selection = ref('');
+    
+    function getAutoCompleteData(term){
+      selection.value = term
+      if(term.length > 2) {
+          axios.get(`/products/get-data?term=${term}`)
+            .then(res=>{
+                suggestions.value = res.data
+            }) 
+          }
+    }
+
+    function updateCurrentList(index){
+      const suggestion = suggestions.value.filter(list => list.id == index)
+      selection.value = suggestion[0].title;
+      suggestions.value= [];
+      Inertia.visit(`products/${suggestion[0].id}`)
+    }
+    // console.log(tabs.selected)
     return {
       tabs,
+      selection,
+      updateCurrentList,
+      getAutoCompleteData,
+      suggestions,
     }
   },
 };
