1ae544a532b4209faefc86da29137f2488004d7a
Ukpe Sophia Abel <54262136+SophiannaRoxanna@users.noreply.github.com>
2021-11-15 19:39:28 +0100

    created the customers table and create customers modal which is currently a page

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -1,48 +1,102 @@
 <template>
-  <app-layout>
-    <div class="flex-1 flex flex-col overflow-y-auto xl:overflow-hidden">
-      <!-- Breadcrumb -->
-      <nav
-        aria-label="Breadcrumb"
-        class="bg-white border-b border-blue-gray-200 xl:hidden"
-      >
-        <div class="max-w-3xl mx-auto py-3 px-4 flex items-start sm:px-6 lg:px-8">
-          <inertia-link href="#" class=" -ml-1 inline-flex items-center space-x-3 text-sm font-medium text-blue-gray-900">
-            <ChevronLeftIcon
-              class="h-5 w-5 text-blue-gray-400"
-              aria-hidden="true"
-            />
-            <span>Add a new Customer</span>
-          </inertia-link>
-        </div>
-      </nav>
+    <app-layout>
+        <div class="flex-1 flex flex-col overflow-y-auto xl:overflow-hidden">
+            <!-- Breadcrumb -->
+            <nav
+                aria-label="Breadcrumb"
+                class="bg-white border-b border-blue-gray-200 xl:hidden"
+            >
+                <div
+                    class="
+                        max-w-3xl
+                        mx-auto
+                        py-3
+                        px-4
+                        flex
+                        items-start
+                        sm:px-6
+                        lg:px-8
+                    "
+                >
+                    <inertia-link
+                        href="#"
+                        class="
+                            -ml-1
+                            inline-flex
+                            items-center
+                            space-x-3
+                            text-sm
+                            font-medium
+                            text-blue-gray-900
+                        "
+                    >
+                        <ChevronLeftIcon
+                            class="h-5 w-5 text-blue-gray-400"
+                            aria-hidden="true"
+                        />
+                        <span>Add a new Customer</span>
+                    </inertia-link>
+                </div>
+            </nav>
 
-      <div class="flex-1 flex xl:overflow-hidden">
-        <!-- Secondary sidebar -->
+            <div class="flex-1 flex xl:overflow-hidden">
+                <!-- Secondary sidebar -->
 
-        <!-- Main content -->
-        <div class="flex-1 max-h-screen xl:overflow-y-auto">
-          <div class="flex justify-between mt-5 mx-5">
-               <h1 class="text-2xl font-extrabold text-blue-gray-900">
-                Add New Customer
-              </h1>
-              <span>
-                <button class="px-6 py-3 text-center border border-indigo-600 text-indigo-600 cursor-pointer rounded-md">Cancel</button>
-                <button class="px-6 py-3  text-center border border-gray-400 cursor-pointer bg-indigo-600 text-white  rounded-md ml-4" @click="submitForm">Add Customer</button>
-              </span>
-            </div>
-          <div class="max-w-3xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8">
-            
-           
-            <div class="bg-white pb-10 pt-1 mt-4">
-              <personal-info-form :info="info"></personal-info-form>
-              <div class="border-b border-gray-200"></div>
-              <address-form
-                :address="address"
-                :countries="countries"
-                @added="addTag"
-              ></address-form>
-              <!-- <div class="text-center bg-white pt-6 pb-6 mb-6">
+                <!-- Main content -->
+                <div class="flex-1 max-h-screen xl:overflow-y-auto">
+                    <div
+                        class="
+                            max-w-3xl
+                            mx-auto
+                            py-8
+                            px-4
+                            sm:px-6
+                            lg:py-12 lg:px-8
+                        "
+                    >
+                        <div class="bg-white pb-10 pt-1 mt-4">
+                            <personal-info-form
+                                :info="info"
+                            ></personal-info-form>
+                            <div class="border-b border-gray-200"></div>
+                            <address-form
+                                :address="address"
+                                :countries="countries"
+                                @added="addTag"
+                            ></address-form>
+
+                            <span class="flex justify-between mt-7.5 mx-8">
+                                <button
+                                    class="
+                                        px-6
+                                        py-3
+                                        text-center
+                                        border border-indigo-600
+                                        text-indigo-600
+                                        cursor-pointer
+                                        rounded-md
+                                    "
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    class="
+                                        px-6
+                                        py-3
+                                        text-center
+                                        border border-gray-400
+                                        cursor-pointer
+                                        bg-indigo-600
+                                        text-white
+                                        rounded-md
+                                        ml-4
+                                    "
+                                    @click="submitForm"
+                                >
+                                    Save
+                                </button>
+                            </span>
+                            <!-- <div class="text-center bg-white pt-6 pb-6 mb-6">
                 <t-button
                   class="
                     text-white
@@ -58,27 +112,28 @@
                   >Add Customer</t-button
                 >
               </div> -->
+                        </div>
+                    </div>
+                </div>
+                <!-- <Nav page="General"></Nav> -->
             </div>
-            <span class="flex justify-between mt-5">
-              <button class="px-6 py-3 text-center border border-indigo-600 text-indigo-600 cursor-pointer rounded-md">Cancel</button>
-              <button class="px-6 py-3  text-center border border-gray-400 cursor-pointer bg-indigo-600 text-white  rounded-md ml-4" @click="submitForm">Add Customer</button>
-            </span>
-          </div>
         </div>
-        <!-- <Nav page="General"></Nav> -->
-      </div>
-    </div>
-  </app-layout>
+    </app-layout>
 </template>
 
 <script>
-import { ref,} from "vue";
+import { ref } from "vue";
 import AppLayout from "../../Layouts/AppLayout.vue";
 // import Search from "../Search.vue";
 import Nav from "../../Layouts/Nav";
 // import axios from "axios";
 
-import { Dialog,DialogOverlay,TransitionChild,TransitionRoot} from "@headlessui/vue";
+import {
+    Dialog,
+    DialogOverlay,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
 
 import UploadIcon from "../../../assets/UploadIcon";
 import AngleUpIcon from "../../../assets/AngleUpIcon";
@@ -89,109 +144,111 @@ import Multiselect from "@vueform/multiselect";
 import PersonalInfoForm from "./Components/PersonalInfoForm";
 import AddressForm from "./Components/AddressForm";
 // import SideNav from './../../Layouts/SideNav'
-import useVuelidate from '@vuelidate/core'
+import useVuelidate from "@vuelidate/core";
 
 const statusStyles = {
-  success: "bg-green-100 text-green-800",
-  processing: "bg-yellow-100 text-yellow-800",
-  failed: "bg-gray-100 text-gray-800",
+    success: "bg-green-100 text-green-800",
+    processing: "bg-yellow-100 text-yellow-800",
+    failed: "bg-gray-100 text-gray-800",
 };
 export default {
-  props: {
-    countries: Array,
-  },
-
-  components: {
-    Nav,
-    AppLayout,
-    Dialog,
-    DialogOverlay,
-    TransitionChild,
-    TransitionRoot,
-    Multiselect,
-    UploadIcon,
-    AngleUpIcon,
-    PersonalInfoForm,
-    AddressForm,
-  },
-
-  data() {
-    return {
-      v$: useVuelidate(),
-      formFields: {
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone: "",
-      },
-      address: {
-        first_name: "",
-        last_name: "",
-        state: null,
-        city: null,
-        postal_code: "",
-        address: "",
-        apartment: "",
-        country: null,
-      },
-      info: {
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone_number: "",
-      },
-      formFields_errors: {
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone: "",
-      },
-      address_errors: {
-        first_name: "",
-        last_name: "",
-        state: null,
-        city: null,
-        postal_code: "",
-        address: "",
-        apartment: "",
-        country: null,
-      },
-      info_errors: {
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone_number: "",
-      },
-    };
-  },
-  computed: {
-    calculateMargin() {
-      this.formFields.margin = 0;
-      return `$ ${0}`;
+    props: {
+        countries: Array,
     },
-  },
-  methods: {
-    submitForm() {
-      this.v$.$validate()
-     
-      // console.log({...this.address, ...this.info});
-      if(!this.v$.$error){
-        this.$inertia.post("/customers", {...this.address, ...this.info});
-      }
+
+    components: {
+        Nav,
+        AppLayout,
+        Dialog,
+        DialogOverlay,
+        TransitionChild,
+        TransitionRoot,
+        Multiselect,
+        UploadIcon,
+        AngleUpIcon,
+        PersonalInfoForm,
+        AddressForm,
     },
-  },
-  setup() {
-    const open = ref(false);
-    return { statusStyles, }
-  },
+
+    data() {
+        return {
+            v$: useVuelidate(),
+            formFields: {
+                first_name: "",
+                last_name: "",
+                email: "",
+                phone: "",
+            },
+            address: {
+                first_name: "",
+                last_name: "",
+                state: null,
+                city: null,
+                postal_code: "",
+                address: "",
+                apartment: "",
+                country: null,
+            },
+            info: {
+                first_name: "",
+                last_name: "",
+                email: "",
+                phone_number: "",
+            },
+            formFields_errors: {
+                first_name: "",
+                last_name: "",
+                email: "",
+                phone: "",
+            },
+            address_errors: {
+                first_name: "",
+                last_name: "",
+                state: null,
+                city: null,
+                postal_code: "",
+                address: "",
+                apartment: "",
+                country: null,
+            },
+            info_errors: {
+                first_name: "",
+                last_name: "",
+                email: "",
+                phone_number: "",
+            },
+        };
+    },
+    computed: {
+        calculateMargin() {
+            this.formFields.margin = 0;
+            return `$ ${0}`;
+        },
+    },
+    methods: {
+        submitForm() {
+            this.v$.$validate();
+
+            // console.log({...this.address, ...this.info});
+            if (!this.v$.$error) {
+                this.$inertia.post("/customers", {
+                    ...this.address,
+                    ...this.info,
+                });
+            }
+        },
+    },
+    setup() {
+        const open = ref(false);
+        return { statusStyles };
+    },
 };
 </script>
 <style scoped>
 @import "style.css";
 .quill {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 </style>
 <style src="@vueform/multiselect/themes/default.css"></style>
-
