e411419d1826855051e81475389d81b476c7fddd
Aderibigbe Toyese <toyeseaderibigbe@gmail.com>
2021-11-27 20:58:16 +0100

    merge conflict resolved

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,17 +11,6 @@ import "./index.css"
 import "../js/Components/Tooltip/Styles/main.css";
 import directives from "../js/Components/Tooltip/Directives/index";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas);
-import { fab } from '@fortawesome/free-brands-svg-icons';
-library.add(fab);
-import { far } from '@fortawesome/free-regular-svg-icons';
-library.add(far);
-import { dom } from "@fortawesome/fontawesome-svg-core";
-dom.watch();
-
 const el = document.getElementById('app');
 // register all directives
 const test = createApp(InertiaApp);
@@ -35,20 +24,11 @@ createApp({
         resolveComponent: (name) => require(`./Pages/${name}`).default,
     }),
 })
-<<<<<<< HEAD
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .component('QuillEditor', QuillEditor)
-    .component('vueDebounce', vueDebounce)
-    .component("font-awesome-icon", FontAwesomeIcon)
-    .mount(el);
-=======
 .mixin({ methods: { route } })
 .use(InertiaPlugin)
 .component('QuillEditor', QuillEditor)
 .component('vueDebounce', vueDebounce)
 .mount(el);
 
->>>>>>> 6090e2436368f456484638dae58e2e6bf1b511bd
 
 InertiaProgress.init({ color: '#4B5563' });
