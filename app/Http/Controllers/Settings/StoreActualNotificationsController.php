abc4ad027b78c944c84e377c90f500ab8e728a69
Atam Jacob <jacob.atam@gmail.com>
2021-12-24 07:31:21 -0800

    Added notifications

--- app/Http/Controllers/Settings/StoreActualNotificationsController.php
+++ app/Http/Controllers/Settings/StoreActualNotificationsController.php
@@ -29,6 +29,7 @@ class StoreActualNotificationsController extends Controller
         $store_actual_notifications = new StoreActualNotification;
         $store_actual_notifications->subject = $request->subject;
         $store_actual_notifications->notification = $request->message;
+        $store_actual_notifications->store_notification_id = $request->store_notification_id;
         $store_actual_notifications->store_id =  $user->store_id;
         $store_actual_notifications->user_id  =   $user->id;
         if ( $store_actual_notifications->save() ) {
