b20181b89d31b1b7e2ac7d041b886862d1663c50
Atam Jacob <jacob.atam@gmail.com>
2022-03-01 04:04:35 -0800

    Updated delete

--- app/Http/Controllers/Settings/StoreLocationController.php
+++ app/Http/Controllers/Settings/StoreLocationController.php
@@ -150,7 +150,7 @@ class StoreLocationController extends Controller
             if(null !== $location) {
                 if($location->delete()) {
                     Log::info('User ' . Auth::id() . ' deleted a store location ' . $id);
-                    return response()->json(['message' => 'Deleted successfully'], 422);
+                    return response()->json(['message' => 'Deleted successfully'], 200);
                 }else{
                     Log::error('User ' . Auth::id() . ' could not delete a store location ' . $id);
                     return response()->json('Resource could not be deleted', 422);
