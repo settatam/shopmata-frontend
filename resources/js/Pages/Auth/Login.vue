6d1e77dfe076e372032e8edecd5f55ae1e8024f4
Atam Jacob <jacob.atam@gmail.com>
2022-03-03 07:00:52 -0800

    Added forgot password

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -29,9 +29,9 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link  href="/password/reset" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?llll
-                </Link>
+                <inertia-link v-if="canResetPassword" href="/password/reset" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Forgot your password?
+                </inertia-link>
 
                 <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
@@ -49,6 +49,7 @@
     import JetCheckbox from '@/Jetstream/Checkbox'
     import JetLabel from '@/Jetstream/Label'
     import JetValidationErrors from '@/Jetstream/ValidationErrors'
+    import { Link } from '@inertiajs/inertia-vue3'
 
 
     export default {
