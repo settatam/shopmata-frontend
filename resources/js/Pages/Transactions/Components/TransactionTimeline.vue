c12062497169ffb5127b26faf07a140223e8e247
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2022-03-13 22:44:48 +0400

    transaction timeline ui fixes

--- resources/js/Pages/Transactions/Components/TransactionTimeline.vue
+++ resources/js/Pages/Transactions/Components/TransactionTimeline.vue
@@ -1,64 +1,76 @@
 <template>
-    <div class=" rounded-md bg-white ml-3 mr-3">
+    <div class=" rounded-md bg-white">
         <div class="rounded-t-md w-full bg-purple-darken py-4 pl-4 text-white">
             <h1 class="text-white">Transaction Timeline</h1>
         </div>
 
-        <div class="flex flex-row space-x-2 justify-between mx-4">
-            <div class="w-1/2 my-4 space-x-2 flex flex-row">
+        <div
+            class="flex flex-col md:flex-col lg:flex-row lg:space-x-2 justify-between mx-4"
+        >
+            <div class=" my-4 flex flex-row">
                 <label class="pt-2" for="">Status: </label>
-                <div>
-                    <input
-                        type="search"
-                        :disabled="true"
-                        class="py-2 text-sm text-black rounded-md pl-10 focus:outline-none focus:bg-white w-96 disabled:bg-gray-background"
-                        placeholder="Kit Received"
-                        autocomplete="off"
-                    />
-                    <p class="text-xs text-red-400">Delete transaction</p>
-                </div>
-                <div>
-                    <button
-                        class="bg-purple-darken px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken w-40"
-                        type="submit"
-                    >
-                        Confirm Status
-                    </button>
+                <div class="flex flex-col lg:flex-row ">
+                    <div>
+                        <input
+                            type="search"
+                            :disabled="true"
+                            class="py-2 text-sm text-black rounded-md focus:outline-none focus:bg-white disabled:bg-gray-background mx-2"
+                            placeholder="Kit Received"
+                            autocomplete="off"
+                        />
+                        <p class="text-xs my-2 mx-2 text-red-400">
+                            Delete transaction
+                        </p>
+                    </div>
+                    <div>
+                        <button
+                            class="bg-purple-darken py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken mx-2 lg:mx-0 px-2"
+                            type="submit"
+                        >
+                            Confirm Status
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <div class="w-1/2 my-4 space-x-2 flex flex-row">
-                <label class="mt-2" for="">Offer: </label>
-                <div>
-                    <input
-                        type="search"
-                        class="py-2 text-sm text-black rounded-md pl-10 focus:outline-none focus:bg-white w-96"
-                        placeholder=""
-                        autocomplete="off"
-                    />
-                </div>
-                <div>
-                    <button
-                        class="bg-purple-darken px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
-                        type="submit"
-                    >
-                        Send Offer
-                    </button>
+            <div class=" my-4 flex flex-row">
+                <label class="pt-2" for="">Status: </label>
+                <div class="flex flex-col lg:flex-row ">
+                    <div>
+                        <input
+                            type="search"
+                            :disabled="true"
+                            class="py-2 text-sm text-black rounded-md focus:outline-none focus:bg-white disabled:bg-gray-background mx-2"
+                            placeholder="Kit Received"
+                            autocomplete="off"
+                        />
+                        <p class="text-xs my-2 mx-2 text-red-400">
+                            Delete transaction
+                        </p>
+                    </div>
+                    <div>
+                        <button
+                            class="bg-purple-darken py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken mx-2 lg:mx-0 px-2"
+                            type="submit"
+                        >
+                            Confirm Status
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="flex flex-row text-black">
-            <div class="flex flex-col p-4 space-y-3 w-1/2">
+        <div class="flex flex-col md:flex-row text-black text-xs md:text-sm">
+            <div class="flex flex-col p-4 space-y-3 lg:w-1/2">
                 <p class="text-green-darker">Kit requested</p>
                 <p class="text-green-darker">Kit sent</p>
                 <p class="text-green-darker">Package Received</p>
                 <p>Offer given</p>
                 <p>Offer accepted</p>
-                <p>PAyment processed</p>
+                <p>Payment processed</p>
             </div>
 
-            <div class="flex flex-col w-1/2 mx-4">
+            <div class="flex flex-col lg:w-1/2 mx-4">
                 <div class="my-2 mx-1 flex flex-row space-x-4">
                     <textarea
                         class="shadow-sm block sm:text-sm border-gray-300 rounded-md "
@@ -69,10 +81,10 @@
                         cols="60"
                     ></textarea>
 
-                    <div class="flex flex-col space-y-2">
+                    <div class="flex flex-col space-y-2 w-1/2 lg:full ">
                         <div>
                             <button
-                                class="bg-purple-darken w-40 px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
+                                class="bg-purple-darken w-40 px-2 md:px-6 py-2 border border-transparent rounded-md shadow-sm text-xs md:text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
                                 type="submit"
                             >
                                 Print Labels
@@ -81,7 +93,7 @@
 
                         <div>
                             <button
-                                class="bg-purple-darken w-40 px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
+                                class="bg-purple-darken w-40 px-2 md:px-6 py-2 border border-transparent rounded-md shadow-sm md:text-sm text-xs font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
                                 type="submit"
                             >
                                 Print Barcodes
@@ -102,7 +114,7 @@
                         cols="60"
                     ></textarea>
 
-                    <div class="flex flex-col space-y-2">
+                    <div class="flex flex-col space-y-2 w-1/2 lg:full">
                         <div>
                             <button
                                 class="bg-purple-darken w-40 px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken "
