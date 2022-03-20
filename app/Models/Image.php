6f831c437496e22dca5d02ca2061ba4e61d14276
Atam Jacob <jacob.atam@gmail.com>
2022-03-20 07:57:24 -0700

    Refactor code

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -9,19 +9,8 @@ class Image extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     protected $fillable = [ 'url', 'rank', 'image','imageable_type','imageable_id'];
 
-=======
-    protected $table = "images";
->>>>>>> c0d8e66d12e483896e1d5baa33fcdac26f3f7c1e
-=======
-    protected $table = "images";
-  
-    protected $fillable = [ 'url', 'rank', 'image','imageable_type','imageable_id'];
-
->>>>>>> 41eb7df445eeae94d26ea7eca82ba6e0cab35d20
 
     public function imageable()
     {
--- app/Models/TransactionHistory.php
+++ app/Models/TransactionHistory.php
@@ -25,10 +25,4 @@ class TransactionHistory extends Model
     const OFFER_DECLINED = 'OFFER DECLINED';
     const OFFER_PAID = 'OFFER PAID';
 
-<<<<<<< HEAD
-
-   
-
-=======
->>>>>>> 41eb7df445eeae94d26ea7eca82ba6e0cab35d20
 }
