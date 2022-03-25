00d0fee9c9b3a3a832673c1efbe6f804e35ff575
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2022-03-25 17:52:57 +0400

    create reports page

---
+++ resources/js/Pages/Reports/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <app-layout>
+        <!-- Page header -->
+        <div id="container" class="flex flex-col mx-3"></div>
+    </app-layout>
+</template>
+<script>
+import { ref, reactive } from 'vue'
+import AppLayout from '../../Layouts/AppLayout.vue'
+import axios from 'axios'
+import { SearchIcon, PlusIcon } from '@heroicons/vue/solid'
+import { MailIcon, PhoneIcon } from '@heroicons/vue/outline'
+import Pagination from '../../Components/Pagination.vue'
+import { Inertia } from '@inertiajs/inertia'
+const statusStyles = {
+    success: 'bg-green-100 text-green-800',
+    processing: 'bg-yellow-100 text-yellow-800',
+    failed: 'bg-gray-100 text-gray-800'
+}
+export default {
+    components: {
+        AppLayout,
+        SearchIcon
+    },
+    props: {
+        notifications: Array,
+        transactions: Object
+    }
+}
+</script>
