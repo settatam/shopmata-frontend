1d5ff97e3919102f6a3bbded75eb94e0cb4d5cad
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2022-03-29 01:28:29 +0400

    sort table functionality

--- resources/js/Pages/Reports/Components/Entries.vue
+++ resources/js/Pages/Reports/Components/Entries.vue
@@ -18,7 +18,7 @@
                 </select>
             </div>
 
-            <div class="my-2">
+            <div class="my-2 ">
                 <button
                     class="bg-purple-darken px-2 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken inline-flex items-center"
                     type="submit"
@@ -28,7 +28,7 @@
             </div>
         </div>
 
-        <table class="min-w-full">
+        <table class="min-w-full table-fixed">
             <thead class=" bg-purple-darken text-white table-auto border-b">
                 <tr>
                     <th
@@ -37,6 +37,7 @@
                     >
                         #Tx ID
                         <SwitchVerticalIcon
+                            @click="sortBy('txId')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
@@ -46,6 +47,7 @@
                     >
                         Bought
                         <SwitchVerticalIcon
+                            @click="sortBy('bought')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
@@ -55,6 +57,7 @@
                     >
                         Estimated Value
                         <SwitchVerticalIcon
+                            @click="sortBy('estimatedValue')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
@@ -64,6 +67,7 @@
                     >
                         Customer ID
                         <SwitchVerticalIcon
+                            @click="sortBy('customerId')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
@@ -73,58 +77,64 @@
                     >
                         # of Trans
                         <SwitchVerticalIcon
+                            @click="sortBy('ofTrans')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                     <th
-                        class="hidden lg:table-cell text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
+                        class="text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
                         scope="col"
                     >
                         Status
                         <SwitchVerticalIcon
+                            @click="sortBy('status')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                     <th
-                        class="hidden lg:table-cell text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
+                        class="text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
                         scope="col"
                     >
                         Cstomer Since
                         <SwitchVerticalIcon
+                            @click="sortBy('customerSince')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                     <th
-                        class="hidden lg:table-cell text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
+                        class="text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
                         scope="col"
                     >
                         Source
                         <SwitchVerticalIcon
+                            @click="sortBy('source')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                     <th
-                        class="hidden lg:table-cell text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
+                        class="text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
                         scope="col"
                     >
                         Keyword
                         <SwitchVerticalIcon
+                            @click="sortBy('keyword')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                     <th
-                        class="hidden lg:table-cell text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
+                        class="text-xs lg:text-sm font-bold px-5 mr-1 py-1 text-left"
                         scope="col"
                     >
                         Lead
                         <SwitchVerticalIcon
+                            @click="sortBy('lead')"
                             class="h-5 w-5 ml-1 inline-flex cursor-pointer"
                         />
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="item in table" :key="item.index">
+                <tr v-for="item in sortedValues" :key="item.index">
                     <td
                         class="text-xs text-purple-darken lg:text-sm font-light px-6 py-4 whitespace-nowrap"
                     >
@@ -146,34 +156,34 @@
                         {{ item.customerId }}
                     </td>
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class=" text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.ofTrans }}
                     </td>
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class=" text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.status }}
                     </td>
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class=" text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.customerSince }}
                     </td>
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class="text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.source }}
                     </td>
 
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class="text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.keyword }}
                     </td>
 
                     <td
-                        class="hidden lg:table-cell text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
+                        class=" text-xs lg:text-sm text-gray-600 font-light px-6 py-4 whitespace-nowrap"
                     >
                         {{ item.lead }}
                     </td>
@@ -184,7 +194,7 @@
 </template>
 
 <script>
-import { reactive, ref } from '@vue/reactivity'
+import { reactive, ref, computed } from '@vue/reactivity'
 import { SwitchVerticalIcon } from '@heroicons/vue/solid'
 
 export default {
@@ -192,11 +202,11 @@ export default {
         SwitchVerticalIcon
     },
     setup () {
-        const table = reactive([
+        const tableData = reactive([
             {
                 txId: 9859,
-                bought: 0.00,
-                estimatedValue: 0.00,
+                bought: 0.0,
+                estimatedValue: 0.0,
                 customerId: 4360,
                 ofTrans: 1,
                 status: 'Kit-Sent',
@@ -207,8 +217,8 @@ export default {
             },
             {
                 txId: 9591,
-                bought: 0.00,
-                estimatedValue: 0.00,
+                bought: 0.0,
+                estimatedValue: 0.0,
                 customerId: 4168,
                 ofTrans: 2,
                 status: 'Kit-Sent',
@@ -231,8 +241,8 @@ export default {
             },
             {
                 txId: 9600,
-                bought: 4200.00,
-                estimatedValue: 4200.00,
+                bought: 4200.0,
+                estimatedValue: 4200.0,
                 customerId: 4360,
                 ofTrans: 4,
                 status: 'Payment Processed ',
@@ -254,7 +264,38 @@ export default {
                 lead: 'google-CPC'
             }
         ])
-        return { table }
+        const currentSort = ref('txId')
+        const reverse = ref(false)
+        const currentSortDir = ref('asc')
+
+        function sortBy (s) {
+            if (s === currentSort.value) {
+                currentSortDir.value =
+                    currentSortDir.value === 'asc' ? 'desc' : 'asc'
+            }
+            currentSort.value = s
+        }
+
+        const sortedValues = computed(() => {
+            return tableData.sort((a, b) => {
+                let modifier = 1
+                if (currentSortDir.value === 'desc') modifier = -1
+                if (a[currentSort.value] < b[currentSort.value])
+                    return -1 * modifier
+                if (a[currentSort.value] > b[currentSort.value])
+                    return 1 * modifier
+                return 0
+            })
+        })
+
+        return {
+            tableData,
+            currentSort,
+            reverse,
+            sortBy,
+            currentSortDir,
+            sortedValues
+        }
     }
 }
 </script>
