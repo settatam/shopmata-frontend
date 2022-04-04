c5b9118c8f789d67e7cdb21427fcc0096ca9e254
Atam Jacob <jacob.atam@gmail.com>
2022-04-04 05:16:55 -0700

    Updated migration

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -63,8 +63,7 @@ class TransactionsController extends Controller
         $transaction = Transaction::find($id);
         $statuses = Status::all();
         $categories = Category::where('store_id',session('store_id'))->get();
-        $transaction->load('customer','customer.state','items','items.images','histories','offers','notes','sms','images', 'activities','items','transaction_payment_address');
-        //dd($transaction);
+        $transaction->load('customer','customer.state','items','items.images','histories','offers','notes','sms','images', 'activities','items','transaction_payment_address','transaction_payment_address.transaction_payment_type');
         return Inertia::render('Transactions/Show', compact('transaction','categories','statuses'));
     }
 
