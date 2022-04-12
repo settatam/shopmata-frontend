f7a9ed66f8df89a386536094b1de0bc8b4df4fd8
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2022-04-12 07:35:47 +0400

    messages scaffolding

---
+++ resources/js/Pages/Messages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <app-layout>
+        <!-- Page header -->
+        <div id="container" class="flex flex-col mx-3 space-y-1">
+            <div>
+                <SearchRow />
+            </div>
+        </div>
+    </app-layout>
+</template>
+<script>
+import { ref, reactive } from 'vue'
+import AppLayout from '../../Layouts/AppLayout.vue'
+import axios from 'axios'
+import { SearchIcon, PlusIcon } from '@heroicons/vue/solid'
+import { Inertia } from '@inertiajs/inertia'
+import SearchRow from './Components/SearchRow.vue'
+
+
+export default {
+    components: {
+        AppLayout,
+        SearchIcon,
+        SearchRow,
+    },
+    props: {
+        notifications: Array,
+        transactions: Object
+    }
+}
+</script>
