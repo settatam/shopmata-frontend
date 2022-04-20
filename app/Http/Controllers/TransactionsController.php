6c915f3e4f185f2aef193c56497614aabf7839f4
Atam Jacob <jacob.atam@gmail.com>
2022-04-20 08:00:30 -0700

    Updated Transaction

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -175,7 +175,7 @@ class TransactionsController extends Controller
 
 
             if ( $transaction ) {
-                Log::info("Note(s) Updated!", );
+                Log::info("Note(s) Updated!" );
                 return response($transaction->load('public_note','private_note'),200);
             }
 
