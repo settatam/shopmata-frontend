1afcf75b6e6a8d44fc4e34025ca07c6f87136656
Atam Jacob <jacob.atam@gmail.com>
2022-04-20 07:49:09 -0700

    Updated Transaction

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -171,6 +171,8 @@ class TransactionsController extends Controller
             );
 
             if ( $transaction ) {
+                $transaction  = Transaction::findorFail($request->transaction_id);
+
                 Log::info("Note(s) Updated!", );
                 return response($transaction->load('public_note','private_note'),200);
             }
