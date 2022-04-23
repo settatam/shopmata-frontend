e76b3925a6d38567b53ba6ab9e2628f9e1b61307
Seth Atam <seth.atam@gmail.com>
2022-04-23 10:17:28 -0400

    FIx

--- app/Http/Middleware/StoreInit.php
+++ app/Http/Middleware/StoreInit.php
@@ -7,6 +7,7 @@ use App\Models\StoreDomain;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
 
 class StoreInit
 {
@@ -19,20 +20,15 @@ class StoreInit
      */
     public function handle(Request $request, Closure $next)
     {
-//        dd(session()->get('store_id'));
-//        $request->session()->forget('store_id');
         if(env('APP_ENV') !== 'development') {
-            //Get
-//            if($request->path() == 'login' || $request->path() == 'register') {
-//                return $next($request);
-//            }
-           $url = URL::to('/');
-           //if(!session()->has('store_id')) {
-               $storeDomain = StoreDomain::where('name', 'http://127.0.0.1:8000')->where('is_active', 1)->first();
-               if(null !== $storeDomain) {
-                   session()->put('store_id', $storeDomain->store_id);
-               }
-           //}
+            if($subdomain = Route::input('subdomain')) {
+                $storeDomain = StoreDomain::where('name', $subdomain)->where('is_active', 1)->first();
+                if(null !== $storeDomain) {
+                    session()->put('store_id', $storeDomain->store_id);
+                }else{
+                    abort(404);
+                }
+            }
         }
         return $next($request);
     }
