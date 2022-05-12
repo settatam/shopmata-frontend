6674af0608594a079e0ea7dbe6840605af32cb8c
Rippyblogger <boyeadeotan@gmail.com>
2022-05-12 13:01:08 +0400

    init

--- resources/js/Pages/Customers/Components/PaymentInformation.vue
+++ resources/js/Pages/Customers/Components/PaymentInformation.vue
@@ -2,7 +2,10 @@
     <div class=" bg-white mt-7 mb-7 mx-auto rounded-md">
         <div class="rounded-t-md w-full bg-purple-darken p-4 text-white">
             <h1 class="text-xl">
-                Payment Information <span class="cursor-pointer" @click="toggleEdit()"> [ Edit ]</span>
+                Payment Information
+                <span class="cursor-pointer" @click="toggleEdit()">
+                    [ Edit ]</span
+                >
             </h1>
         </div>
 
@@ -17,6 +20,19 @@
             <p>ACCOUNT NAME:</p>
             <p>ACCOUNT TYPE:</p>
         </div>
+
+        <div v-else>
+            <div class="p-6 space-x-2">
+                <label class="font-semibold mb-1 bg-transparent" for="pay_method">PAY METHOD:</label>
+                <select class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 w-fit sm:text-sm border-gray-300 rounded-md" name="pay_method" id="" v-model="payment_method">
+                    <option value="choose">Choose Method</option>
+                    <option value="check">Check</option>
+                    <option value="paypal">PayPal</option>
+                    <option value="ach">ACH</option>
+                    <option value="venmo">Venmo</option>
+                </select>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,11 +42,13 @@ import { ref } from 'vue'
 export default {
     setup () {
         const isEdit = ref(false)
-        function toggleEdit(){
+        function toggleEdit () {
             isEdit.value = !isEdit.value
         }
 
-        return { isEdit, toggleEdit }
+        const payment_method = ref('choose')
+
+        return { isEdit, toggleEdit, payment_method }
     }
 }
 </script>
