00b92d3fa8e49f6c9f3c0461ad6bd6563652638b
Rippyblogger <boyeadeotan@gmail.com>
2022-05-12 12:49:03 +0400

    toggle edit

--- resources/js/Pages/Customers/Components/PaymentInformation.vue
+++ resources/js/Pages/Customers/Components/PaymentInformation.vue
@@ -1,10 +1,12 @@
 <template>
     <div class=" bg-white mt-7 mb-7 mx-auto rounded-md">
         <div class="rounded-t-md w-full bg-purple-darken p-4 text-white">
-            <h1 class="text-xl">Payment Information</h1>
+            <h1 class="text-xl">
+                Payment Information <span class="cursor-pointer" @click="toggleEdit()"> [ Edit ]</span>
+            </h1>
         </div>
 
-        <div class="p-6 space-y-3 text-gray-lighter">
+        <div v-if="!isEdit" class="p-6 space-y-3 text-gray-lighter">
             <p>IS LOAN:</p>
             <p>PAY METHOD:</p>
             <p>
@@ -15,10 +17,20 @@
             <p>ACCOUNT NAME:</p>
             <p>ACCOUNT TYPE:</p>
         </div>
-
     </div>
 </template>
 
 <script>
-export default {}
+import { ref } from 'vue'
+
+export default {
+    setup () {
+        const isEdit = ref(false)
+        function toggleEdit(){
+            isEdit.value = !isEdit.value
+        }
+
+        return { isEdit, toggleEdit }
+    }
+}
 </script>
