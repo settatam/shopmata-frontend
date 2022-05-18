b7dbcd8a52265b94118b56a21bcabb72aef6ae30
Seth Atam <seth.atam@gmail.com>
2022-05-18 18:14:44 -0400

    Fix

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -14,8 +14,21 @@ class ReportsController extends Controller
     public function index(Request $request)
     {
         $filters = $request->input();
+        $statuses = Transaction::searchForFilter($filters)->with('trStatus')->withGroupedStatus()->get();
+        $genders = Transaction::searchForFilter($filters)->withGroupedGender()->get();
+        $repeatCustomerCount = Transaction::searchForFilter($filters)->withGroupedRepeatCustomer(true)->count();
+        $noRepeatCustomerCount = Transaction::searchForFilter($filters)->withGroupedRepeatCustomer(false)->count();
+        $repeatCustomers = collect([
+            'Yes' => $repeatCustomerCount,
+            'No' => $noRepeatCustomerCount
+        ]);
+
+        
+
         //†$filters['dates'] = Filter::dates($filters);
         $filters['type'] = 'ReportsTable';
-        return Inertia::render('Reports/Index', compact('filters'));
+        return Inertia::render('Reports/Index',
+            compact('filters', 'statuses', 'genders', 'repeatCustomers')
+        );
     }
 }
