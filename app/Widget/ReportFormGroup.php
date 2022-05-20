fcd500f3efbb58a6b26656a81276fb4030cbadff
Atam Jacob <jacob.atam@gmail.com>
2022-05-20 10:48:28 -0700

    Updated reports

--- app/Widget/ReportFormGroup.php
+++ app/Widget/ReportFormGroup.php
@@ -4,6 +4,16 @@ namespace App\Widget;
 
 use App\Widget\StatusCheckBoxes;
 use App\Widget\RepeatCustomerCheckBoxes;
+use App\Widget\LeadTypeCheckBoxes;
+use App\Widget\PaymentTypeCheckBoxes;
+//use App\Widget\BehaviourCheckBoxes;
+//use App\Widget\TagsCheckBoxes;
+//use App\Widget\SourceCheckBoxes;
+//use App\Widget\AgeCheckBoxes;
+use App\Widget\DaysCheckBoxes;
+use App\Widget\StatesCheckBoxes;
+
+
 
 
 class ReportFormGroup extends Form
@@ -25,7 +35,35 @@ class ReportFormGroup extends Form
             ],
             [
                 'label' => 'Lead Type',
-                'fields' => (new RepeatCustomerCheckBoxes())->render($filter)
+                'fields' => (new LeadTypeCheckBoxes())->render($filter)
+            ],
+            [
+                'label' => 'Payment Type',
+                'fields' => (new PaymentTypeCheckBoxes())->render($filter)
+            ],
+            // [
+            //     'label' => 'Behaviour',
+            //     'fields' => (new BehaviourCheckBoxes())->render($filter)
+            // ],
+            // [
+            //     'label' => 'Tags',
+            //     'fields' => (new TagsCheckBoxes())->render($filter)
+            // ],
+            // [
+            //     'label' => 'Source',
+            //     'fields' => (new SourceCheckBoxes())->render($filter)
+            // ],
+            // [
+            //     'label' => 'Age',
+            //     'fields' => (new AgeCheckBoxes())->render($filter)
+            // ],
+            [
+                'label' => 'Days',
+                'fields' => (new DaysCheckBoxes())->render($filter)
+            ],
+            [
+                'label' => 'States',
+                'fields' => (new StatesCheckBoxes())->render($filter)
             ]
         ];
     }
