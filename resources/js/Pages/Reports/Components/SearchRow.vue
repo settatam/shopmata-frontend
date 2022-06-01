9259bf688f3ec9cc92df86dfa143311e0c1e6844
Rippyblogger <boyeadeotan@gmail.com>
2022-06-01 14:50:23 +0400

    reports page

--- resources/js/Pages/Reports/Components/SearchRow.vue
+++ resources/js/Pages/Reports/Components/SearchRow.vue
@@ -1,86 +1,84 @@
 <template>
-    <div
-        class="grid grid-cols-2 gap-2 my-4 md:px-0 md:grid-cols-6 rounded bg-transparent"
-    >
+    <div class="grid grid-cols-2 gap-2 my-4 md:px-0 md:grid-cols-7 rounded bg-transparent">
+        <!-- <div class="flex flex-row my-4 md:px-0 rounded bg-transparent space-x-6 flex-wrap"> -->
+        <button type="button"
+            class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-darken py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-purple-darken focus:ring-offset-2 sm:w-auto"
+            @click="filterToggle">
+            Filter by:
+        </button>
+
         <div class="">
-            <div
-                class="relative w-full text-gray-400 focus-within:text-gray-600"
-            >
-                <div
-                    class="absolute inset-y-0 left-0 flex items-center pointer-events-none"
-                    aria-hidden="true"
-                >
-                    <SearchIcon class="h-5 w-5 ml-5" aria-hidden="true" />
-                </div>
-                <input
-                    id="search-field"
-                    name="search-field"
-                    class="rounded text-xm md:text-base text-gray-500 w-full md:search-field md:w-full pl-12 pr-2 bg-white border border-gray-200 placeholder-gray-500 placehol"
-                    placeholder="Search Reports"
-                    type="search"
-                />
-            </div>
+            <input type="date" name=""
+                class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200" id="" />
         </div>
 
         <div class="">
-            <input
-                type="date"
-                name=""
-                class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200"
-                id=""
-            />
-        </div>
-
-        <div class="">
-            <input
-                type="date"
-                name=""
-                class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200"
-                id=""
-            />
+            <input type="date" name=""
+                class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200" id="" />
         </div>
 
         <div class="flex flex-col">
-            <select
-                name="Status"
-                id=""
-                class="rounded text-gray-500 text-xm md:text-base w-full bg-white border border-gray-200"
-            >
+            <select name="Status" id=""
+                class="rounded text-gray-500 text-xm md:text-base w-full bg-white border border-gray-200">
                 <option value="">Date Kit Requested</option>
             </select>
         </div>
         <div class="flex flex-col">
             <div class="flex">
-                <select
-                    name="Tag"
-                    id=""
-                    class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200"
-                >
+                <select name="Tag" id=""
+                    class="rounded text-xm md:text-base text-gray-500 w-full bg-white border border-gray-200">
                     <option value="active">BuyMyGold.com</option>
                 </select>
             </div>
         </div>
 
-        <div class="flex">
+        <!-- <div class="flex">
             <div class="flex flex-row space-x-2">
                 <button
                     class="bg-purple-darken px-2 py-1 border border-transparent rounded shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken inline-flex items-center"
-                    type="submit"
-                >
+                    type="submit">
                     Apply
                 </button>
 
                 <button
                     class="bg-purple-darken px-2 py-1 border border-transparent rounded shadow-sm text-sm font-medium text-white hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-darken inline-flex items-center"
-                    type="submit"
-                >
+                    type="submit">
                     New Customer
                 </button>
             </div>
-        </div>
+        </div> -->
+
+            <button
+                class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-darken py-2 px-2 text-sm font-medium text-white shadow-sm hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-purple-darken focus:ring-offset-2 sm:w-auto"
+                type="submit">
+                Apply
+            </button>
+
+            <button
+                class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-darken py-2 px-2 text-sm font-medium text-white shadow-sm hover:bg-purple-darken focus:outline-none focus:ring-2 focus:ring-purple-darken focus:ring-offset-2 sm:w-auto"
+                type="submit">
+                New Customer
+            </button>
+
     </div>
+
+    <Filter class="my-3" v-if="displayFilter" />
 </template>
 
 <script>
-export default {}
+import { ref } from "vue";
+import Filter from "../../../Components/Filter.vue";
+export default {
+    components: {
+        Filter,
+    },
+    setup() {
+        const displayFilter = ref(false);
+        function filterToggle() {
+            displayFilter.value = !displayFilter.value;
+        }
+
+        return { displayFilter, filterToggle };
+    },
+};
 </script>
