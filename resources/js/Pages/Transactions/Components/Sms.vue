237afe30d0e53318a870cde363ec4fc56432df52
Rippyblogger <boyeadeotan@gmail.com>
2022-06-07 17:05:16 +0400

    sms

--- resources/js/Pages/Transactions/Components/Sms.vue
+++ resources/js/Pages/Transactions/Components/Sms.vue
@@ -48,8 +48,6 @@
                             class="block w-full border-0 py-2 resize-none placeholder-gray-500 focus:ring-0 sm:text-sm"
                             v-model="smsMessage" placeholder="Write a description..." />
 
-                        <images-list />
-
                         <!-- Spacer element to match the height of the toolbar -->
                         <div aria-hidden="true">
                             <div class="py-2">
@@ -79,7 +77,7 @@
                                     <PaperClipIcon class="-ml-1 h-5 w-5 mr-2 group-hover:text-gray-500"
                                         aria-hidden="true" />
                                     <!-- </button> -->
-                                    <input @change="uploadImage($event)" type="file" class="hidden " id="file"
+                                    <input @change="previewImages($event)" type="file" class="hidden " id="file"
                                         name="image" accept="image/gif,image/jpeg,image/jpg,image/png" multiple=""
                                         data-original-title="upload photos">
                                 </label>
@@ -93,9 +91,9 @@
                         </div>
                     </div>
                 </form>
+
+                <images-list class="mx-3" :images="images" v-if="images.length" @image-deleted="delete_img" />
             </div>
-
-
 
 
             <div class="flex justify-center">
@@ -116,6 +114,7 @@ import ImagesList from '../../../Components/ImageList.vue'
 import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
 import { CalendarIcon, PaperClipIcon, TagIcon, UserCircleIcon } from '@heroicons/vue/solid'
 import fileUploader from "../../../Utils/fileUploader";
+import urls from '../../api/urls'
 
 export default {
     components: { AppLayout, Button, Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions, CalendarIcon, PaperClipIcon, TagIcon, UserCircleIcon, ImagesList },
@@ -126,24 +125,21 @@ export default {
         const buttonName = ref('Send Message')
         const loadingAnimation = ref(false)
         const url = "/admin/images";
-        const previewImage = ref(null)
+        let images = ref([])
         const { saveFiles } = fileUploader();
 
-        // test
-        function uploadImage(event) {
-            /* let data = new FormData();
-            data.append('file', event.target.files[0]); */
-
-            // const image = event.target.files[0];
-            // const reader = new FileReader(image);
-            // reader.readAsDataURL(image);
-            // reader.onload = event => {
-            //     previewImage.value = event.target.result;
-            //     console.log(this.previewImage);
-            // };
+        function previewImages(event) {
             let acceptFiles = event.target.files
-            saveFiles(Array.from(acceptFiles), {model: 'sms', transaction_id: 12803}, null)
-            .then((res)=>console.log(res))
+            saveFiles(Array.from(acceptFiles))
+                .then((res) => {
+                    images.value = res.data
+                    console.log(images.value)
+                })
+                .catch((err) => console.log(err))
+        }
+
+        function delete_img(index) {
+            images.value.splice(index)
         }
 
         // test
@@ -162,7 +158,9 @@ export default {
 
         function addMessage() {
             loadingAnimation.value = true
-            axios.post('/admin/transactions/' + props.id + '/sms', {
+            uploadImage()
+
+            axios.post(urls.transactions.sms(props.id), {
                 message: smsMessage.value
             }).then((res) => {
                 loadingAnimation.value = false
@@ -176,7 +174,7 @@ export default {
             })
         })
 
-        return { smsTimes, formatDate, formattedTimes, smsMessage, addMessage, buttonName, loadingAnimation, url, uploadImage, previewImage }
+        return { smsTimes, formatDate, formattedTimes, smsMessage, addMessage, buttonName, loadingAnimation, url, images, previewImages, delete_img }
     }
 }
 </script>
