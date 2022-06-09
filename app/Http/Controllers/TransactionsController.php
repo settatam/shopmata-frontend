7fc5aff06e5f9fcde12548d49f8d0790128fe45a
Seth Atam <seth.atam@gmail.com>
2022-06-09 11:41:23 -0400

    Fix store data

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -424,7 +424,7 @@ class TransactionsController extends Controller
     {
         //
         $input = $request->input();
-        $transaction = Transaction::find($id);
+        $transaction = Transaction::withFinalOffer()->find($id);
 
         //How do we perform validation here???
 
