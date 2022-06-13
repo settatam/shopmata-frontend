e75c06ec1649f5b5ae5c81fe76b59b3cb0280d7d
Seth Atam <seth.atam@gmail.com>
2022-06-13 16:58:35 -0400

    Fix

--- app/Http/Controllers/Auth/CustomerLoginController.php
+++ app/Http/Controllers/Auth/CustomerLoginController.php
@@ -53,6 +53,8 @@ class CustomerLoginController extends Controller
             'password' => ['required', 'string'],
         ]);
 
+        dd($request->session()->get('store_id'));
+
         $customer = Customer::where('email', $request->email)
             ->where('store_id', $request->session()->get('store_id'))
             ->first();
