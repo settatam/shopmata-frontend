34d57f3194fb1eae9eb6371c21e3ebea5be25732
Seth Atam <seth.atam@gmail.com>
2022-06-14 09:24:13 -0400

    Fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -28,7 +28,7 @@ class VerifyCsrfToken extends Middleware
     {
         $token = $this->getTokenFromRequest($request);
 
-        dd($request->header());
+        dd($token);
 
         return is_string($request->session()->token()) &&
                is_string($token) &&
