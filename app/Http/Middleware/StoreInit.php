87669ce8a694ca1d78020dc5c7a896be22458867
Seth Atam <seth.atam@gmail.com>
2022-06-18 14:24:42 -0400

    Fix

--- app/Http/Middleware/StoreInit.php
+++ app/Http/Middleware/StoreInit.php
@@ -31,7 +31,6 @@ class StoreInit
 
             $url = URL::to('/');
             $storeDomain = StoreDomain::with('store')->where('name', $url)->first();
-            dd($storeDomain);
 
             if(null !== $storeDomain) {
                 $store = $storeDomain->store;
