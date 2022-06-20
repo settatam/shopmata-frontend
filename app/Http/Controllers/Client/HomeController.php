82a0617c045ab483285c3037a46e61cbaa216c10
Seth Atam <seth.atam@gmail.com>
2022-06-20 17:12:18 -0400

    Fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -35,6 +35,7 @@ class HomeController extends Controller
             $pageType = 'page';
 
             $data = [];
+            $data['store'] = $store;
 
             if($pageToFind == 'transactions') {
 
