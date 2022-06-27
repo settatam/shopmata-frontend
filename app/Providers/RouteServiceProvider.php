72d7cab7057f5bcf425ee818fb56779a00da9b1b
Seth Atam <seth.atam@gmail.com>
2022-06-27 17:39:21 -0400

    Fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/transactions';
 
     /**
      * The controller namespace for the application.
