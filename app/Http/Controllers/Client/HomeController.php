918f75d98c52913630d17603da53e1c1b8d22cca
Seth Atam <seth.atam@gmail.com>
2022-11-07 11:57:49 -0500

    Fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -379,6 +379,7 @@ class HomeController extends Controller
   public function updateAddressVerification (Request $request)
   {
     $transaction = Transaction::find($request->session()->get('transactionId'));
+    dd($transaction);
     $address = $request->session()->get('verifiedAddress');
     //update verified address
     if (null !== $transaction) {
