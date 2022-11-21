5a2aaa23da844f8dbf3c8412ed8cb4c06f859d6e
Seth Atam <seth.atam@gmail.com>
2022-11-21 14:23:17 -0500

    Fix

--- app/Http/Controllers/API/TransactionsController.php
+++ app/Http/Controllers/API/TransactionsController.php
@@ -35,40 +35,88 @@ class TransactionsController extends Controller
 
     public function verifyAddress(Request $request) {
 
-        $request->validate([
-            //'email' => ['required','email','max:75'],
-        ]);
+    $request->validate([
+      //'email' => ['required','email','max:75'],
+    ]);
 
-        $store_id = $request->store_id ?? 43;
+    $input = $request->input();
 
-        $store = Store::find($store_id);
-        $customer = new Customer;
-        //try {
-            $input = $request->input();
-            $input['first_name'] = $request->first_name ?? $request->firstname;
-            $input['last_name'] = $request->last_name ?? $request->lastname;
-            $customer = Customer::addNew($store, $input);
-            Auth::guard('customer')->loginUsingId($customer->id);
+    $customerAddress = [
+      'first_name' => data_get($input, 'firstname'),
+      'last_name' => data_get($input, 'lastname'),
+      'address' => data_get($input, 'address'),
+      'address2' => data_get($input, 'apt'),
+      'state' => data_get($input, 'state'),
+      'city' => data_get($input, 'city'),
+      'country_id' => 1,
+      'zip' => data_get($input, 'zip'),
+      'phone' => data_get($input, 'phone'),
+      'state_id' => Helper::getStateId(data_get($input, 'state'))
+    ];
 
-            $transaction = Transaction::createNew($store, $request, $customer);
-            $transaction_payment_address = new TransactionPaymentAddress;
-            $transaction_payment_address = TransactionPaymentAddress::firstOrNew(
-                ['transaction_id' => $transaction->id ]
-            );
-            $transaction_payment_address->transaction_id         =  $transaction->id;
-            $transaction_payment_address->customer_id            =  $customer->id;
-            $transaction_payment_address->payment_type_id        =  $request->payment;
-            $transaction_payment_address->save();
-                
-    
-            return response()->json(null, 200);
-//        } catch (\Throwable $th) {
-//            \Log::Error("Failed to save  transaction  with" . collect($request->all())  ."  Error: " .$th->getMessage() );
-//            return response()->json(['message'=> "Failed to save  transaction"], 422);
-//        }
+    $address = new Address();
+    $address->fill($customerAddress);
 
-
+    try {
+      $fedex = new Fedex();
+      $addressVerification = $fedex->verifyAddress($address);
+      $request->session()->put('verifiedAddress', $addressVerification);
+    } catch (\Exception $e) {
+      $addressVerification['valid'] = false;
     }
+
+    if(!data_get($addressVerification, 'valid')) {
+      return response()->json($addressVerification);
+    }
+
+    $store_id = session()->get('store_id');
+
+    $store = Store::find($store_id);
+    $customer = new Customer();
+    //try {
+
+    $input['first_name'] = $request->first_name ?? $request->firstname;
+    $input['last_name'] = $request->last_name ?? $request->lastname;
+
+    $customer = Customer::where('email', $input['email'])->first();
+
+    $customerAddress['address'] = $addressVerification['parsedAddress']['street'];
+    $customerAddress['address2'] = $addressVerification['parsedAddress']['street2'];
+    $customerAddress['state'] = Helper::getStateId($addressVerification['parsedAddress']['state']);
+    $customerAddress['city'] = $addressVerification['parsedAddress']['city'];
+    $customerAddress['zip'] = $addressVerification['parsedAddress']['zip'];
+
+    if(null === $customer) {
+      $customer = Customer::addNew($store, $input);
+    } else {
+      $customer->addresses()->delete();
+    }
+
+    $cAddress = $customer->address()->firstOrNew($customerAddress);
+    $cAddress->save();
+
+    if(!Auth::check()) {
+      Auth::loginUsingId($customer->id);
+    }
+
+    $transaction = Transaction::createNew($store, $request, $customer);
+    $transaction->address()->create($customerAddress);
+    $addressVerification['transaction_id'] = $transaction->id;
+
+    $transaction_payment_address = new TransactionPaymentAddress();
+    $transaction_payment_address = TransactionPaymentAddress::firstOrNew(
+      [ 'transaction_id' => $transaction->id ]
+    );
+
+    $transaction_payment_address->transaction_id = $transaction->id;
+    $transaction_payment_address->customer_id = $customer->id;
+    $transaction_payment_address->payment_type_id = $request->pay;
+
+    $transaction_payment_address->save();
+
+    return response()->json($addressVerification);
+
+  }
 
     /**
      * Store a newly created resource in storage.
