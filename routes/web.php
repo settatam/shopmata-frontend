91fabee6e890024425f17bdc294a02bc9e43601f
Seth Atam <seth.atam@gmail.com>
2023-05-05 09:22:22 -0400

    Fix

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::get('customer/login-with-token', [CustomerLoginController::class, 'loginW
 //Route::domain('{account}.'.env('APP_URL'))->group(function () {
     Route::get('transactions/{id}/track-my-kit', [PDFController::class, 'trackKit']);
     Route::get('my-account.html', [ClientHomeController::class, 'index']);
+    Route::get('track-my-kit.html', [ClientHomeController::class, 'index']);
     Route::get('/', [ClientHomeController::class, 'index']);
     Route::get('transactions', [ClientHomeController::class, 'index']);
     Route::get('transactions/{id}', [ClientHomeController::class, 'index']);
