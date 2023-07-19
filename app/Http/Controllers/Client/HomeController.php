afd39a9accc502843ebd1ee812c4dda2ed6d8877
Seth Atam <seth.atam@gmail.com>
2023-07-19 15:57:44 -0400

    Fix issues

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -28,7 +28,6 @@ class HomeController extends Controller
   public function index(Request $request, $id=null)
   {
     //
-
     if(session()->has('store_id')) {
       //This is probably a store page
       $store_id = session()->get('store_id');
