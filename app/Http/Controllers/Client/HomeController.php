6c0164b7519fd34ba5861a110ea226caca7bc5be
Seth Atam <seth.atam@gmail.com>
2023-08-10 15:38:51 -0400

    Fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -539,7 +539,7 @@ class HomeController extends Controller
             $img = Image::make($images[0]);
             $pathToSave = sprintf('%s/%s', public_path(), $imageName);
             $img->save($pathToSave);
-            $generated_images[] = sprintf('%s/%s', url()->current(), $imageName);
+            $generated_images[] = sprintf('%s/%s', url()->basePath(), $imageName);
           }
           data_set($input, 'generated_images', $generated_images);
         }
