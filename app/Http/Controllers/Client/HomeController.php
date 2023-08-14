8ed2d4ac455bce05173e69c275c183b28d78a41b
Seth Atam <seth.atam@gmail.com>
2023-08-14 18:23:30 -0400

    Fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -546,8 +546,8 @@ class HomeController extends Controller
           data_set($input, 'generated_images', $generated_images);
         }
       } else {
-        $newContent = unserialize($tracking->content);
         dd($newContent);
+        $newContent = unserialize($tracking->content);
         $generated_images = data_get($newContent, 'generated_images', []);
         data_set($input, 'generated_images', $generated_images);
       }
