b8e01619056ba3da88aa1a7cdce6e5d503d26a7e
Seth Atam <seth.atam@gmail.com>
2023-10-05 17:13:14 -0400

    Fix activities delete

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -77,6 +77,7 @@ class HomeController extends Controller
 
           if (null !== $transaction->tracking) {
             $transaction->tracking()->delete();
+            $transaction->activities()->where('status', 'Pending Kit Request - Incomplete')->where('is_status', 1)->delete();
           }
           $clientId = $request->session()->get('google-seo-client-id');
           $sessionId = $request->session()->get('google-seo-session-id');
