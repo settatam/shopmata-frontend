5a2d0ed4f9bbc47c485787439ddad46bc4e7f918
Seth Atam <seth.atam@gmail.com>
2025-10-08 01:45:27 -0400

    Fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -32,7 +32,6 @@ class HomeController extends Controller
             $sortBy = $request->sort_by ?? 'created_at';
 
             $pageToFind = StorePage::nameFromPath($path);
-            dd($pageToFind);
             $pageType = 'page';
 
             $data = [];
